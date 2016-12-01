38e8590153ed16058bb4c9639fc65e694a5f8db3
Ubuntu <ubuntu@ip-172-31-43-59.us-west-2.compute.internal>
2016-12-01 05:32:40 +0000

    karta customized

--- wp-content/themes/karta/index.php
+++ wp-content/themes/karta/index.php
@@ -19,35 +19,46 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php if ( have_posts() ) : ?>
-
 			<?php if ( ! is_paged() ) : ?>
 			<div class="latest-posts">
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-12">
-
+			
 			<?php else : ?>
 			<div class="container">
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="masonry-grid">
-								<div class="masonry-grid__sizer"></div>
-								<div class="masonry-grid__gutter-sizer"></div>
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="masonry-grid">
+							<div class="masonry-grid__sizer"></div>
+							<div class="masonry-grid__gutter-sizer"></div>
+			
 			<?php endif; ?>
 
 			<?php
 			$post_counter = 0;
 			while ( have_posts() ) : the_post();
-
-				if ( $post_counter < 3 && ! is_paged() ) : ?>
-					<?php get_template_part( 'template-parts/content-latest' ); ?>
+				// 將 $post_counter 改為判斷<1（只有第一則為latest-posts__post）
+				if ( $post_counter < 1 && ! is_paged() ) :
+					get_template_part( 'template-parts/content-latest' ); ?>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="masonry-grid">
+									<div class="masonry-grid__sizer"></div>
+									<div class="masonry-grid__gutter-sizer"></div>
+					<?php ++$post_counter;
+					continue; ?>
 
 				<?php else : ?>
 					<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 					
-				<?php endif; // Endif counter < 3. ?>
-
-				<?php if ( ! is_paged() && 3 === ++$post_counter && $wp_query->post_count > 3 ) : ?>
+				<?php endif; ?>
+				
+				<?php if ( ! is_paged() && 3 === (++$post_counter-1) && $wp_query->post_count > 3 ) : ?>
 						</div>
 					</div>
 				</div>
